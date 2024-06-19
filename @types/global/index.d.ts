2992049eed56b5c3778a5de1180edeedf44982f7
allready-dawn <qwshpke@naver.com>
2024-06-19 17:47:22 +0900

    Login Page View

---
+++ @types/global/index.d.ts
@@ -0,0 +1,3 @@
+// png, svg파일 인식
+declare module '*.png';
+declare module '*.svg';
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Apptest from './AppTest';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./views/Autentication/SignUp";
+import InputBox from "./components/inputBox/Index";
+import { ChangeEvent, useState } from "react";
 
 function App() {
-  return (
-    <div>
-        <Apptest/>
-    </div>
-  );
+
+    const [id, setId] = useState<string>('');
+
+    const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setId(value);
+    }
+
+    const onIdButtonClickHandler = () => {
+
+    }
+
+    return (
+        <Routes>
+            <Route path="/auth">
+                <Route path="sign-up" element={<SignUp />} />
+                <Route path="sign-up-test" element={<InputBox title="아이디" placeholder="아이디를 입력해주세요" type="text" value={id} onChange={onIdChangeHandler} buttonTitle="중복 확인" onButtonClick={onIdButtonClickHandler} message="사용 가능한 아이디입니다." isErrorMessage={true}/>}/>
+                <Route path="sign-up-button" element=<div className="text-link-lg  full-width">회원가입</div> />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
